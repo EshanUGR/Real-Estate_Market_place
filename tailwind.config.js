88f9d291a10ea85347a56b288c2fb50b6a31afc2
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2024-07-26 12:48:37 +0530

    install tailwind css and react first template

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // Adjust paths based on your project structure
+    './public/index.html', // Include other paths as necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
