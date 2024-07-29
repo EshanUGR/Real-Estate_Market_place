fc0e725270f6db5b95f9179eafa747fdb43906f9
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2024-07-29 19:51:41 +0530

    connected MongoDB database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 // 47.22
 
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>
+{
+  console.log("Connected to MongoDB");
+})
+.catch((err)=>
+{
+  console.log(err);
+})
 const app = express();
 let PORT = 3000;
+
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}!!`);
