fa8600754f8b5886cf67ed3020ea8ca6f6fe9477
Ravindu Eshan <127772297+EshanUGR@users.noreply.github.com>
2024-07-31 18:04:11 +0530

    create signup api routes

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,8 @@
+export const test=(req,res)=>
+
+  (req,res)=>
+{
+  res.json({
+    message:'Api is working'
+  });
+}
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
-// 47.22
+// 1.20
 
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>
 {
@@ -13,9 +15,25 @@ mongoose.connect(process.env.MONGO).then(()=>
   console.log(err);
 })
 const app = express();
+
+app.use(express.json());
 let PORT = 3000;
 
 
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}!!`);
 });
+
+
+app.get('/test',(req,res)=>
+{
+ res.json({
+  message:'Hello World!'
+ });
+})
+
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+
+
